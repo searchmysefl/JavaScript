03cbd01717aa8693ea0c5e1f0118e3508d4b3cf8
Ivan <example@gmail.com>
2020-08-01 21:51:41 +0700

    Первый коммит

---
+++ js/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// alert('Hello');
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = prompt("Вам есть 18?", "нет ответа");
+// console.log(answer);
+
+const answers = [];
+
+answers[0] = prompt("Как ваше имя?");
+answers[1] = prompt("Как ваша фамилия?");
+answers[2] = prompt("Сколько вам лет?");
+
+console.log(answers);
